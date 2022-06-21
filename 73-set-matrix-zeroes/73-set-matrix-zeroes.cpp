220bce8904c0fdf5dd1633ce1ca9a41bec7ffca9
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-06-21 23:53:49 +0530

    Time: 123 ms (5.72%), Space: 13.2 MB (81.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,35 +1,70 @@
 class Solution {
 public:
+    
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> v;
         int m=matrix.size();
         int n=matrix[0].size();
+        int firstcol=0;
+        
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                firstcol=1;
+            }
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        
+        
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[0][j]==0 || matrix[i][0]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
         
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(matrix[i][j]==0)
-                {
-                    v.push_back({i,j});
-                }
-            }
+                cout<<matrix[i][j]<<" ";
+            }cout<<endl;
         }
         
-        for(int k=0;k<v.size();k++)
+        if(matrix[0][0]==0)
         {
-            int r=v[k].first;
-            int c=v[k].second;
-            
-            for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
             {
-                matrix[r][i]=0;
-            }
-            for(int j=0;j<m;j++)
-            {
-                matrix[j][c]=0;
+                matrix[0][j]=0;
             }
         }
+        cout<<endl;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cout<<matrix[i][j]<<" ";
+            }cout<<endl;
+        }
         
+        if(firstcol)
+        {
+             for(int j=0;j<m;j++)
+            {
+                matrix[j][0]=0;
+            }
+        }
     }
 };
