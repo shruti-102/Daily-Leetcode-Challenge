3b8911f0b607eb319fd80b91302a3d8ec8613e33
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-06-30 20:50:28 +0530

    Time: 510 ms (19.45%), Space: 12 MB (52.27%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -8,28 +8,33 @@ public:
             sum+=nums[i];
         }
         if(sum%2) return false;
-        sum=sum/2;
+        int target=sum/2;
         
-        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,0));
+        vector<vector<bool>> dp(n+1,vector<bool>(target+1,0));
         
         for(int i=0;i<n;i++)
         {
             dp[i][0]=true;
         }
+         for(int j=1;j<target;j++)
+        {
+            dp[0][j]=false;
+        }
+        
         
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=sum;j++)
+            for(int j=1;j<=target;j++)
             {
-                if(nums[i-1]>j)
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }
+                bool nottake=dp[i-1][j];
+                
+                bool take=false;
+                if(nums[i-1]<=j)
+                    take=dp[i-1][j-nums[i-1]];
+                dp[i][j]=take | nottake;
             }
         }
-        return dp[n][sum];
+        
+        return dp[n][target];
     }
 };
