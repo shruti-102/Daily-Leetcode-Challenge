b67e3b909005569c26c8e487fafeb88207bdaf1d
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-07-03 01:14:24 +0530

    Time: 11 ms (9.26%), Space: 7.2 MB (33.19%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        //transpose
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<m;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cout<<matrix[i][j]<<" ";
+            }cout<<endl;
+        }
+        //mirror image
+        cout<<endl;
+        for(int j=0;j<m/2;j++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                swap(matrix[i][j],matrix[i][m-j-1]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cout<<matrix[i][j]<<" ";
+            }cout<<endl;
+        }
+    }
+};
