c1c54125d815da44d40b7891919ddb8cdd108378
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-07-06 13:22:50 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (5.26%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,22 +4,53 @@ public:
         
         int n=matrix.size();
         int m=matrix[0].size();
+    
         int targetrow=n-1;
-        for(int i=0;i<n;i++)
+        bool found=false;
+        int l=0,h=n-1;
+        while(l<=h)
         {
-            if(target==matrix[i][0]) return true;
-            
-            if(target<matrix[i][0]){
-                targetrow=i-1;
+            int mid=l+(h-l)/2;
+            cout<<mid<<" ";
+            if(matrix[mid][0]==target)
+            {
+                found=true;
+                targetrow=mid;
                 break;
             }
-        }
+            else if(matrix[mid][0]<target && matrix[mid][m-1]>=target)
+            {
+                cout<<"&&";
+                found=true;
+                targetrow=mid;
+                break;
+            }
+            if(matrix[mid][0]>target)
+            {
+                h=mid-1;
+            }
+            
+            else if(matrix[mid][0]<target)
+            {
+                l=mid+1;
+            }
+        }cout<<endl;
+        cout<<targetrow<<endl;
+        if(!found) return false;
         
-        if(targetrow==-1) return false;
-        
-        for(int j=0;j<m;j++)
+        l=0,h=m-1;
+        while(l<=h)
         {
-            if(matrix[targetrow][j]==target) return true;
+            int mid=l+(h-l)/2;
+            if(matrix[targetrow][mid]==target) return true;
+            else if(matrix[targetrow][mid]>target)
+            {
+                h=mid-1;
+            }
+            else if(matrix[targetrow][mid]<target)
+            {
+                l=mid+1;
+            }
         }
         return false;
     }
