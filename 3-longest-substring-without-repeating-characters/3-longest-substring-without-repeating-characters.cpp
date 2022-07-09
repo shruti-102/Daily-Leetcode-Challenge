c3ec18db6b5c7446eeed9f11efdffb7ddced6fc7
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-07-10 00:19:27 +0530

    Time: 9 ms (86.12%), Space: 8.2 MB (74.38%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-       int n=s.size();
-        unordered_set<int> hashset;
-        int left=0;
-        int maxlen=0;
+        vector <int> mp(256,-1);
+        int n=s.size();
+        if(n==0 || n==1) return n;
+        int left=-1,maxlen=0;
         for(int right=0;right<n;right++)
         {
-            if(hashset.count(s[right]))
+            if(mp[s[right]] > left)  //means that repeated element is in the range[left,right]
             {
-                while(left<right && hashset.find(s[right])!=hashset.end()) //jb tk wo repeated character remove n ho jata
-                {
-                    hashset.erase(s[left]);
-                    left++;
-                }
+                left=mp[s[right]];
+                
             }
-            hashset.insert(s[right]);
-            maxlen=max(maxlen,right-left+1);
+            
+            mp[s[right]]=right;
+            maxlen=max(maxlen,right-left);
         }
+        
         return maxlen;
     }
 };
