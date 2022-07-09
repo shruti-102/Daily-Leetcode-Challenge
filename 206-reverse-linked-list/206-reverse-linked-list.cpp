33795b7609804d17fe59456b93f7e8f31c178dd7
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-07-10 01:50:06 +0530

    Time: 7 ms (79.08%), Space: 8.6 MB (10.56%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -15,6 +15,14 @@ public:
         if(head==NULL) return NULL;
         if(head->next==NULL) return head;
         
+        //newhead will be generated
+        //1->2->3->4->->
+        //this reverseList(2) will return
+        //5->4->3->2 newhead will be 5
+        //now after 2 there should be 1
+        //as 2->1 is not broken yet
+        //head->next->next  1->2->__=head (1)
+        //2->1
         ListNode* newhead=reverseList(head->next);
         head->next->next=head;
         head->next=NULL;
