632d01415ddf96c38e0ec0716f32d426cb39ce7b
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-07-29 04:58:55 +0530

    Time: 474 ms (55.31%), Space: 144.6 MB (22.95%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -6,16 +6,19 @@ public:
         dp[0]=0;
         int ans=INT_MIN;
         
+        //calculate max ele of the array
         for(int i=0;i<n;i++)
         {
             ans=max(ans,nums[i]);
         }
         
+        //pq for getting max previous dp soln in window k
         priority_queue <pair<int,int>> pq;
         pq.push({nums[0],0});
         
         for(int i=1;i<n;i++)
         {
+            //pop until the window sie is not satisfy
             while(i-pq.top().second>k)
             {
                 pq.pop();
