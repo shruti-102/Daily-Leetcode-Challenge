607879d9820b37fb4de67b78e38dd9f4f35f966e
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-08-03 22:36:22 +0530

    Time: 80 ms (14.36%), Space: 71.5 MB (51.04%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,94 +11,54 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans=new ListNode(-1);
-        ListNode* ptr=ans;
-        ListNode* ptr1=l1;
-        ListNode* ptr2=l2;
+        ListNode* temp1=l1;
+        ListNode* temp2=l2;
+        ListNode* newhead=new ListNode(-1);
+        ListNode* prev=newhead;
         int carry=0;
-        while(ptr1!=NULL && ptr2!=NULL)
+        while(temp1!=NULL && temp2!=NULL)
         {
-            int addval=ptr1->val+ptr2->val;
-            if(carry!=0)
-            {
-               addval=addval+carry;
-                if(addval%10 != addval)
-                {
-                    carry=addval/10;
-                    addval=addval%10;
-                }
-                else
-                {
-                    carry=0;
-                }
-            }
-            else
-            {
-                if(addval%10 != addval)
-                {
-                    carry=addval/10;
-                    addval=addval%10;
-                }
-            }
-            
-            ListNode* newnode=new ListNode(addval);
-            ptr->next=newnode;
-            ptr=newnode;
-            ptr1=ptr1->next;
-            ptr2=ptr2->next;
+            int add=temp1->val+temp2->val;
+            add=add+carry;
+            carry=add/10;
+            add=add%10;
+            ListNode* newnode=new ListNode(add);
+            prev->next=newnode;
+            prev=newnode;
+            temp1=temp1->next;
+            temp2=temp2->next;
         }
         
-        while(ptr1!=NULL)
+        while(temp1!=NULL)
         {
-            int addval=ptr1->val;
-            if(carry!=0)
-            {
-                addval+=carry;
-                if(addval%10 != addval)
-                {
-                    carry=addval/10;
-                    addval=addval%10;
-                }
-                else
-                {
-                    carry=0;
-                }
-            }
-            ListNode* newnode=new ListNode(addval);
-            ptr->next=newnode;
-            ptr=newnode;
-            ptr1=ptr1->next;
+            int add=temp1->val;
+            add=add+carry;
+            carry=add/10;
+            add=add%10;
+            ListNode* newnode=new ListNode(add);
+            prev->next=newnode;
+            prev=newnode;
+            temp1=temp1->next;
         }
         
-        while(ptr2!=NULL)
+        while(temp2!=NULL)
         {
-            int addval=ptr2->val;
-            if(carry!=0)
-            {
-                addval+=carry;
-                if(addval%10 != addval)
-                {
-                    carry=addval/10;
-                    addval=addval%10;
-                }
-                else
-                {
-                    carry=0;
-                }
-            }
-            ListNode* newnode=new ListNode(addval);
-            ptr->next=newnode;
-            ptr=newnode;
-            ptr2=ptr2->next;
+            int add=temp2->val;
+            add=add+carry;
+            carry=add/10;
+            add=add%10;
+            ListNode* newnode=new ListNode(add);
+            prev->next=newnode;
+            prev=newnode;
+            temp2=temp2->next;
         }
         
         if(carry)
         {
             ListNode* newnode=new ListNode(carry);
-            ptr->next=newnode;
-            ptr=newnode;
+            prev->next=newnode;
+            prev=newnode;
         }
-        
-        return ans->next;
+        return newhead->next;
     }
 };
