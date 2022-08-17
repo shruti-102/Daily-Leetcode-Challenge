b54717243f2fcd53fb5751c32f1e1f858973b32e
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-08-17 21:56:17 +0530

    Time: 22 ms (95.73%), Space: 13.5 MB (48.21%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,49 +1,45 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        int k=lists.size();
-        if(k==0) return NULL;
-        
-        for(int i=0;i<k;i++){
-            ListNode* temp=lists[i];
-            while(temp!=NULL)
-            {
-                pq.push(temp->val);
-                temp=temp->next;
-            }
-        }
-        
-        ListNode* head=NULL;
-        ListNode* temp=head;
-        
-        while(!pq.empty())
+    
+    ListNode* mergesort(ListNode* a  , ListNode* b) {
+        if(!a) 
+            return b;
+        if(!b)
+            return a;
+        ListNode* ans=NULL;
+        if(a->val<=b->val)
         {
-            if(head==NULL)
-            {
-                ListNode* newnode=new ListNode(pq.top());
-                head=newnode;
-                temp=head;
-                pq.pop();
-            }
-            else
-            {
-                ListNode* newnode=new ListNode(pq.top());
-                temp->next=newnode;
-                temp=newnode;
-                pq.pop();
+            ans=a;
+            ans->next = mergesort(a->next,b);
+        }
+        else
+        {
+            ans=b;
+            ans->next = mergesort(a,b->next);
+        }
+        return ans;
+    }
+    
+    ListNode* mergeK(vector<ListNode*>& lists,int k) {
+        while(k!=0) {
+            int i = 0,j = k;
+            while(i<j) {
+                lists[i]=mergesort(lists[i],lists[j]);
+                i++;
+                j--;
+                if(i>=j) 
+                    k=j;
             }
         }
-        return head;
+        return lists[0];
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==1)
+            return lists[0];
+        if(lists.size()==0) 
+            return NULL;
+        ListNode*newhead = mergeK(lists,lists.size()-1);
+        return newhead;
     }
 };
