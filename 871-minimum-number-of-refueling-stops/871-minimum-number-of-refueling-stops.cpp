193666af02effedee296d8f1b974c0be45397101
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-08-20 14:03:55 +0530

    Time: 136 ms (15.81%), Space: 16.2 MB (51.25%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,3 +1,54 @@
+ /*Try to visualize the problem , as a traditional 
+    Knapsack problem. Now in case of K.S Problems , our 
+    goal is to maximize the profit , within the given 
+    capacity.
+    
+    In these problem , our goal is to Minimize the number
+    of stations picked in our journey to reach the 
+    destination from '0'.
+    
+    So our recursion state would look like something
+    these, dp[i] --> denotes the Minimum number of 
+    stations we need to stop at to reach target.
+    
+    Now , our 'i' value ranges from '0' to 'target'
+    
+    But wait!!! , let's have a look at the constraints ,
+    target <= 10^9 , we can't consider the 'station_dist'
+    as our recursion state , as it will eventually give 
+    us MLE,
+    
+    
+    So , what ......
+    
+    Let's use the same approach , but now from a 
+    different perspective. Instead of finding the minimum
+    number of refueling stations to reach i-th station 
+    ('target') , we just need to find out the maximum 
+    amount of fuel we can get by refuelling at 'i' number
+    of stations.
+    
+    
+    Now , dp[i] --> Maximum amount of fuel , we can 
+    achieve by refueling our car exactly at 'i' number of 
+    stations. 
+    
+    */
+    
+     /*
+     
+     Time Complexity  : O(N*N)
+     Space Complexity : O(N)
+     
+     */
+
+
+
+//dp[i] stores the maximum reach 
+// dp[j]>=(long long)stations[i][0] means
+// suppose 70 tk k reach p pahuch gae bt target 100 h then we need more fuel to reach till target to uske lie jitne piche jo stations chut gae h unme hm dekhege wo maximum fuel wala station lege
+//agr kahi dp[i] m jo reach h uski value target s >= ho gai to i return kr dege
+
 class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
