f71402626e6134100cfa41c733b0b88e2389c537
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-09-14 20:39:48 +0530

    Time: 3 ms (77.21%), Space: 7.6 MB (40.14%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -3,18 +3,24 @@ public:
     
    // consider 1 -> player1
    // consider -1 -> player2
-    int solve(vector<int>& nums,int s,int e,int player)
+    int solve(vector<int>& nums,int s,int e,int player,vector<vector<int>>& dp)
     {
-        if(s==e) return player*nums[s];
-        int choice1=player* nums[s]+ solve(nums,s+1,e,player*-1);
-        int choice2=player*nums[e]+solve(nums,s,e-1,player*-1);
+        if(s==e) return dp[s][e]=player*nums[s];
         
-        if(player<0) return min(choice1,choice2);
-        else return max(choice1,choice2);
+        if(dp[s][e]!=INT_MAX) return dp[s][e];
+        
+        int choice1=player* nums[s]+ solve(nums,s+1,e,player*-1,dp);
+        int choice2=player*nums[e]+solve(nums,s,e-1,player*-1,dp);
+        
+        if(player<0) dp[s][e]=min(choice1,choice2);
+        else dp[s][e]=max(choice1,choice2);
+        
+        return dp[s][e];
     }
     bool PredictTheWinner(vector<int>& nums) {
-        
-        if(solve(nums,0,nums.size()-1,1)>=0) return true;
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int> (n,INT_MAX));
+        if(solve(nums,0,n-1,1,dp)>=0) return true;
         return false;
     }
 };
