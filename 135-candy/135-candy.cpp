3348e1d86c9e2a25f8d04a8b670f0628e8436d7f
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-09-19 02:22:56 +0530

    Time: 64 ms (11.16%), Space: 17.7 MB (79.97%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -3,25 +3,23 @@ public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
         vector<int> dp(n,1);
-        
-        for(int i=0;i<n;i++)
-        {
-            if(i+1<n && ratings[i]>ratings[i+1])
-            {
-                dp[i]=dp[i+1]+1;
-            }
-            if(i-1>=0 && ratings[i]>ratings[i-1])
-            {
-                dp[i]=dp[i-1]+1;
-            }
-        }
-        
         int leftsum=0;
         for(int i=0;i<n;i++)
         {
+            int l=1,r=1;
+            if(i+1<n && ratings[i]>ratings[i+1])
+            {
+                r=dp[i+1]+1;
+            }
+            if(i-1>=0 && ratings[i]>ratings[i-1])
+            {
+                l=dp[i-1]+1;
+            }
+            dp[i]=max(l,r);
             leftsum+=dp[i];
         }
-        dp.clear();
+        
+        int rightsum=0;
         for(int i=n-1;i>=0;i--)
         {
             int left=1,right=1;
@@ -35,12 +33,9 @@ public:
             }
             
             dp[i]=max(left,right);
-        }
-        int rightsum=0;
-        for(int i=0;i<n;i++)
-        {
             rightsum+=dp[i];
         }
+        
         return max(leftsum,rightsum);
     }
 };
