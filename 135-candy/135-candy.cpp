747f1931ff70d7ce2be1bd2f9958102463c38de5
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-09-19 02:33:50 +0530

    Time: 34 ms (57.99%), Space: 17.8 MB (63.23%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int> dp(n,1);
+        vector<int> dp(n);
         int leftsum=0;
         for(int i=0;i<n;i++)
         {
