4c4c31512c9c47ac09e44100ddb97a6279d5f0ba
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-09-19 11:37:28 +0530

    Time: 920 ms (26.39%), Space: 89.9 MB (44.93%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -5,9 +5,7 @@ public:
         int count=0,n=inter.size();
         sort(inter.begin(),inter.end());
         
-        for(int i=0;i<n;i++){
-            cout<<inter[i][0]<<","<<inter[i][1]<<" ";
-        }cout<<endl;
+        
         int e=inter[0][1];
         for(int i=1;i<n;i++){
             if(e>inter[i][0] ){  //overlpping
