0cbe878971e49b7d1df6548d005460341c8fe965
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-09-26 16:20:45 +0530

    Time: 97 ms (11.67%), Space: 25.9 MB (79.78%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,44 +11,19 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& inorder,vector<int>& preorder,unordered_map<int,int>& mpinorder,int instart,int inend,int prestart,int preend)
-    {
-        //base case for index in range
-        if(prestart>preend || instart>inend) return NULL;
+    TreeNode* solve(vector<int>& preorder,int &pre,vector<int>& inorder,int s,int e){
+        if(s>e) return NULL;
+        int i=s;
+        while(inorder[i]!=preorder[pre]) i++;
+        pre++;
         
-        //making newnode with preorder
-        TreeNode* newnode=new TreeNode(preorder[prestart]);
-        
-        //taking root node pinter in inorder
-        int inroot=mpinorder[newnode->val];
-        
-        //calculating number of nodes in the left subtree
-        int x=inroot-instart;
-        
-        //for left subtree
-        //instart=0
-        //inend=rootpointer(inroot)-1 
-        //prestart=prestart+1
-        //preend=prestart+x
-        
-        newnode->left=solve(inorder,preorder,mpinorder,instart,inroot-1,prestart+1,prestart+x);
-        
-        //for right subtree
-        //instart=rootpointer(inroot)+1
-        //inend=inend
-        //prestart=prestart+x+1 (preveious strt + no of nodes + 1)
-        //preend=preend
-        newnode->right=solve(inorder,preorder,mpinorder,inroot+1,inend,prestart+x+1,preend);
-        
+        TreeNode* newnode=new TreeNode(preorder[pre-1]);
+        newnode->left=solve(preorder,pre,inorder,s,i-1);
+        newnode->right=solve(preorder,pre,inorder,i+1,e);
         return newnode;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int,int> mpinorder;
-        for(int i=0;i<inorder.size();i++)
-        {
-            mpinorder[inorder[i]]=i;
-        }
-        
-        return solve(inorder,preorder,mpinorder,0,inorder.size()-1,0,preorder.size()-1);
+        int pre=0;
+        return solve(preorder,pre,inorder,0,inorder.size()-1);
     }
 };
