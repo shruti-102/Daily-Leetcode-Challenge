f378e7050bcc6990fa4dfe848ddee67622e5c6ca
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-09-28 16:13:34 +0530

    Time: 235 ms (5.06%), Space: 69.4 MB (87.80%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int fuel=0,dist=0,n=gas.size();
-        for(int i=0;i<n;i++){
-            fuel+=gas[i];
-            dist+=cost[i];
-        }
-        
-        if(fuel<dist) return -1;
+        int n=gas.size();
         int ans=0,remain=0,total=0;
         
         for(int i=0;i<n;i++){
@@ -18,6 +12,6 @@ public:
             }
             total+=gas[i]-cost[i];
         }
-        return ans;
+        return total<0 ? -1 : ans;
     }
 };
