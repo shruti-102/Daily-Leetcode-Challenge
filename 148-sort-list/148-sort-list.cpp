517fa753873fb1e2d138e5ac60c638ce964d6d7e
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-09-30 21:17:42 +0530

    Time: 251 ms (81.00%), Space: 74.1 MB (15.47%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -18,14 +18,13 @@ public:
             head=head->next->next;
         }
        ListNode* m=mid->next;
-        mid->next=NULL;
+       mid->next=NULL;
         return m;
     }
     
     ListNode* merge(ListNode* left,ListNode* right){
-        ListNode dummy(0);
-        //cout<<dummy->val;
-        ListNode* ptr=&dummy;
+        ListNode* dummy = new ListNode(0);
+        ListNode* ptr = dummy;
         while(left && right){
             if(left->val<right->val) {
                 ptr->next=left;
@@ -41,7 +40,7 @@ public:
         if(left) ptr->next=left;
         else ptr->next=right;
         
-        return dummy.next;
+        return dummy->next;
     }
     ListNode* sortList(ListNode* head) {
         if(!head ||!head->next) return head;
