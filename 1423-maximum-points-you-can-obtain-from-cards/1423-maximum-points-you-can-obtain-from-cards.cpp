9037c33107b1012bc395b80bf7bccdbdde7d2455
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-10-07 20:57:48 +0530

    Time: 155 ms (21.05%), Space: 44 MB (18.98%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int maxScore(vector<int>& card, int k) {
-        int prefixsum=0;
-        for(int i=0;i<k;i++){
-            prefixsum+=card[i];
-        }
-        int n=card.size();
-        int s=k,e=n-1;
-        int maxscore=prefixsum;
-        while(s>0 && s<n){
-            prefixsum-=card[s-1];
-            prefixsum+=card[e];
-            s--;
-            e--;
-            maxscore=max(maxscore,prefixsum);
-        }
-        return maxscore;
+    int maxScore(vector<int>& cardPoints, int k) {
+    int n=cardPoints.size();
+    int maxScore = 0;
+    vector<int> front(k+1), rear(k+1);
+    front[0] = rear[0] = 0;
+
+    // calculate prefix sum of the 1st k elements in front and last k elements in rear
+    // arr: [1,2,3,4,5,6,1], k=3
+    // front: [0,1,3,6], rear: [0,1,7,12]
+    for(int i=0;i<k;i++){
+        front[i+1] = front[i]+cardPoints[i];
+        rear[i+1] = rear[i] + cardPoints[n-1-i];
     }
+
+    // just add relevant prefix sums from front and rear to get the currentScore
+    for(int i=0;i<=k; i++){
+        int currentScore = front[i] + rear[k-i];
+        maxScore = max(maxScore, currentScore);
+    }
+    return maxScore;
+}
 };
