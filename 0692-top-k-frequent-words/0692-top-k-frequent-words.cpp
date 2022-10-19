12510c54c3b6124968ff86c1195de3d9e3b42fec
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-10-19 23:29:28 +0530

    Time: 51 ms (5.81%), Space: 13.6 MB (5.47%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    static bool cmp(pair<string,int> &a, pair<string,int> &b){
+        return a.second>=b.second;
+    }
+    
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        int n=words.size();
+        vector<vector<string>> v(n);
+        
+        unordered_map<string,int> mp;
+        for(auto wrd: words){
+            mp[wrd]++;
+        }
+        
+        map<int,vector<string>, greater<int> > mp2;
+        
+        for(auto it: mp){
+            cout<<it.first<<" ";
+            cout<<it.second<<endl;
+            mp2[it.second].push_back(it.first);
+        }
+        
+       
+        vector<string> ans;
+        
+        for(auto it: mp2){
+            if(k<=0) break;
+            vector<string> temp=it.second;
+            sort(temp.begin(),temp.end());
+            int c=0;
+            while(k>0 && c<temp.size()){
+                ans.push_back(temp[c]);
+                c++;
+                k--;
+            }
+        }
+        return ans;
+    }
+};
