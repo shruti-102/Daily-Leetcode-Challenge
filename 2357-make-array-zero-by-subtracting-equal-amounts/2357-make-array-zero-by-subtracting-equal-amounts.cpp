499f0c2a730b2b280ddf6bedebc196e8ec97bf18
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-10-22 16:49:49 +0530

    Time: 7 ms (35.04%), Space: 8.5 MB (37.21%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int minop=0,minval=INT_MAX,n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0 && nums[i]<minval) minval=nums[i];
-        }
-        cout<<minval;
-        while(true){
-            int count=0;
-            for(int i=0;i<n;i++){
-                if(nums[i]!=0) count++;
-            }
-            if(count==0) break;
-            int newmin=INT_MAX;
-            for(int i=0;i<n;i++){
-                if(nums[i]>0) {
-                    nums[i]=nums[i]-minval;
-                    if(nums[i]>0) newmin=min(newmin,nums[i]);
-                }
-            }
-            minval=newmin;
-            minop++;
-        }
-        return minop;
-    }
+    unordered_set<int> s;
+    for(auto i:nums) if(i!=0) s.insert(i);
+    return s.size();
+}
 };
