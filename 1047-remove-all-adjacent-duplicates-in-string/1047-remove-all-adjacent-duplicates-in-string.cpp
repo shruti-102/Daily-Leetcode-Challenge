9661d7628e3b46333cf6007066e23c65d307a894
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-11-10 11:11:38 +0530

    Time: 44 ms (64.31%), Space: 11.5 MB (24.65%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(st.empty()) st.push(s[i]);
+            else if(st.top()==s[i]) st.pop();
+            else st.push(s[i]);
+        }
+        string res="";
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
