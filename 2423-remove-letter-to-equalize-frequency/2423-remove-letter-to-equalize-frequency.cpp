cd2d006064be4efcf392cc8cc596d217e6788222
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2022-12-14 22:05:02 +0530

    Time: 3 ms (55.93%), Space: 6.2 MB (73.45%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-       bool isEqual(string str, int i)
-    {
-        str.erase(i,1);  //erasing i'th element 
-        unordered_map<char, int> m; 
-
-        //store frequency of character
-        for (int i = 0; i < str.size(); i++)  m[str[i]]++;
-
-        //if the occurence are different we return 0
-        for (int i = 1; i < str.size(); i++)
-            if (m[str[i]] != m[str[i-1]])  return 0;
-
-        return 1;
-    }
     bool equalFrequency(string word) {
-        for (int i = 0; i < word.size(); i++)
-            if (isEqual(word, i)) return true;		
-        return false;
+        vector<int> freq(26); // maintain frequency of every character
+        
+        for(char ch : word) {
+            freq[ch - 'a'] += 1;
+        }
+        
+        int mx_freq = 0, mn_freq = word.size(), cnt_mx = 0, cnt_mn = 0;
+        
+        for(int i = 0; i < 26; ++i) {
+            mx_freq = max(mx_freq, freq[i]);                  // maintain max frequency
+            if(freq[i] > 0) mn_freq = min(mn_freq, freq[i]);  // maintain min frequency
+        }
+        
+        for(int i = 0; i < 26; ++i) {
+            if(freq[i] == mx_freq)
+                ++cnt_mx;         // increment count of characters with max frequency
+            else if(freq[i] == mn_freq)
+                ++cnt_mn;        // increment count of characters with min frequency
+            else if(freq[i] > 0) 
+                return false;   // distinct frequencies is greater than 2
+        }
+        
+        if(mx_freq == mn_freq)
+            return cnt_mx == 1 || mx_freq == 1;
+        
+        if(cnt_mn == 1 && mn_freq == 1)
+            return true;
+        
+        if(mx_freq != mn_freq + 1 || cnt_mx != 1)
+            return false;
+        
+        return true;
     }
 };
