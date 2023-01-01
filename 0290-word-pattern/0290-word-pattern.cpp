fd3ca03d59eeb3e89f1357b84aa410185c97daa9
shruti-102 <78803969+shruti-102@users.noreply.github.com>
2023-01-01 13:51:01 +0530

    Time: 4 ms (32.33%), Space: 6.3 MB (82.12%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    bool wordPattern(string p, string s) {
-        map<char,vector<int>> mp1;
-       map<string,vector<int>> mp2;
-        int n1=p.size();
-        for(int i=0;i<n1;i++){
-            mp1[p[i]].push_back(i);
+    bool wordPattern(string pattern, string s) {
+          vector<string> words;
+    string temp = "";
+    for(int i=0;i<s.length();i++){
+        if(s[i] == ' '){
+            words.push_back(temp);
+            temp = "";
         }
-        int n2=s.size();
-        int k=0,j;
-        string str="";
-        for(int i=0;i<n2;i++){
-            str="";
-            for(j=i;j<n2;j++){
-                if(s[j]==' '){
-                    mp2[str].push_back(k);
-                    k++;
-                    break;
-                }
-                else
-                    str+=s[j];
-            }
-            i=j;
+        else{
+            temp.push_back(s[i]);
         }
-        if(n1!=k+1) return false;
-        mp2[str].push_back(k);
-        
-        for(auto it1: mp1){
-            vector<int> v1=it1.second;
-            for(auto it2:mp2){
-                vector<int> v2=it2.second;
-                if(v1==v2){
-                    mp2.erase(it2.first);
-                    break;
-                }
-            }
-        }
-        if(mp2.size()==0) return true;
+    }
+    words.push_back(temp);
+    if(pattern.length()<words.size() || pattern.length()>words.size()){
         return false;
+    }
+    unordered_map<char,string> m;
+    for(int i=0;i<pattern.size();i++){
+        if(m.find(pattern[i])==m.end()){
+            m[pattern[i]] = words[i];
+        }
+        else if(m[pattern[i]] != words[i]){
+            return false;
+        }
+    }
+    unordered_set<string> set;
+    for(auto it:m){
+        set.insert(it.second);
+    }
+    if(m.size()!=set.size()) return false;
+    return true;
+
     }
 };
